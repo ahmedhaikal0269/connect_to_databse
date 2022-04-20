ba495f10b7351efa6f4cd5511137832aca18efec
ahmed <ahaikal0269@westfield.ma.edu>
2022-04-20 03:10:35 -0400

    Initial commit

---
+++ src/main/java/com/example/connect_to_databse/ConnectToDatabseApplication.java
@@ -0,0 +1,13 @@
+package com.example.connect_to_databse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectToDatabseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectToDatabseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/connect_to_databse/ConnectToDatabseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.connect_to_databse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectToDatabseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
